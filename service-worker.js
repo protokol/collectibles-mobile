0e21e873dfd181bde7177320c05483c581c5d7a6
lubejmatej <lubejmatej@users.noreply.github.com>
2021-02-05 14:07:53 +0000

    deploy: f65c8a99c6b04891ccae72cc916b129f2d93ee9d

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bfc4228204b6349ba79f801f1690802.js"
+  "/collectibles-mobile/precache-manifest.95fc8a91317712dd992a0e907de305ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/collectibles-mobile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
