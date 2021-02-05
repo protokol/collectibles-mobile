d23714c3e709b0ab443460619d0e2f855dd36918
lubejmatej <54802172+lubejmatej@users.noreply.github.com>
2021-02-05 14:47:09 +0100

    chore: add pull request github action (#8)
    * chore: add pull request github action
    
    * wip
    
    * chore: ci add android config
    
    * wip
    
    * chore: dev artifacts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import configureStore from "./store/configure-store";
+import {Provider} from "react-redux";
+
+const store = configureStore();
+
 
 test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+    const {baseElement} = render(<Provider store={store}><App/></Provider>);
+    expect(baseElement).toBeDefined();
 });
