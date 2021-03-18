918ec968c65a438773a9d7c4252bb3c64c1a9f4a
Matej Lubej <matej@protokol.com>
2021-03-18 14:27:35 +0100

    test: mock window matchMedia

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,11 +5,33 @@ import configureStore from './store/configure-store';
 
 const store = configureStore();
 
-test('renders without crashing', () => {
-  const { baseElement } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  expect(baseElement).toBeDefined();
+describe('App', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: () => {
+        return {
+          matches: false,
+          addListener: () => {},
+          removeListener: () => {},
+        };
+      },
+    });
+
+    Object.defineProperty(window, 'getComputedStyle', {
+      value: () => {
+        return {
+          getPropertyValue: () => {},
+        };
+      },
+    });
+  });
+
+  test('renders without crashing', () => {
+    const { baseElement } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(baseElement).toBeDefined();
+  });
 });
