411cff1dd4f9ca5bc7e8d9fa7dfb5f9e7418ec0c
Carlos Mayora <cmayorga@buclelabs.com>
2021-04-27 14:50:43 +0200

    style: added auctions submenu and first list of cards

--- src/components/AuctionSell.tsx
+++ src/components/AuctionSell.tsx
@@ -1,13 +1,29 @@
-import { FC } from 'react';
+import { FC, useCallback, useContext, useEffect, useMemo } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import styled from 'styled-components';
-import { IonCol, IonGrid, IonRow } from '@ionic/react';
-import Button from '../components/ionic/Button';
-import Label from '../components/ionic/Label';
-import Text from '../components/ionic/Text';
+import {
+  IonCol,
+  IonFooter,
+  IonGrid,
+  IonRow,
+  IonSpinner,
+  IonToolbar,
+} from '@ionic/react';
+import Card from './Card';
+import { JSX } from '@ionic/core';
+import Button from './ionic/Button';
+import Label from './ionic/Label';
+import Text from './ionic/Text';
 import { FontSize } from '../constants/font-size';
 import { FontWeight } from '../constants/font-weight';
 import { flagImage } from '../constants/images';
+import useIsMounted from '../hooks/use-is-mounted';
+import useMediaQuery from '../hooks/use-media-query';
+import { CardTagType } from './CardTag';
+import { AuthLoginContext } from '../providers/AuthLoginProvider';
+import { CollectiblesLoadAction } from '../store/actions/collections';
+import { collectionSelector } from '../store/selectors/collections';
 
 const ImageBgCol = styled(IonCol)`
   position: relative;
@@ -67,10 +83,107 @@ const ActionButton = styled(Button)`
   }  
 `;
 
+const CardContainer = styled(IonCol)` 
+  padding: 1.375rem;
+
+  &:nth-child(odd) {
+    padding-left: 0.6875rem;
+  }
+  &:nth-child(even) {
+    padding-right: 0.6875rem;
+  }
+`;
+
+const StartAuctionButton = styled(Button)<JSX.IonButton>`
+  text-decoration: underline;
+  text-transform: capitalize;
+`;
+
+const CardsOnAuctionButton = styled(Button)<JSX.IonButton>`
+  text-decoration: none;
+  text-transform: capitalize;
+`;
+
+const BiddedCardsButton = styled(Button)<JSX.IonButton>`
+  text-decoration: none;
+  text-transform: capitalize;
+`;
+
+const CollectablesIonRow = styled(IonRow)`
+  overflow: auto;
+  max-height: calc(100vh - 92px - 4.8rem);
+`;
+
 const AuctionSell: FC = () => {
-  const history = useHistory();   
-  // console.log("Loading AuctionSell");
+    const history = useHistory();
+    const isMedium = useMediaQuery('(min-height: 992px)');
+    const isLarge = useMediaQuery('(min-height: 1200px)');
+    const { isError, error, isLoading, assets, isLastPage, query } = useSelector(
+      collectionSelector,
+      shallowEqual
+    );
+
+    const StartAuction = () => {
+    }
+
+    const CardsOnAuction = () => {
+    }
+
+    const BiddedCards = () => {
+    }
+
+    const dispatch = useDispatch();
+    const {
+      session: { publicKey },
+    } = useContext(AuthLoginContext);
+  
+    // TBD const publicKeyIn = publicKey;
+    const publicKeyIn = "02cb8f39ec7962ff009626f53326d93bdbb866383f027159a1c63845606f02d31d";  
+  
+    const isMounted = useIsMounted();
+    useEffect(() => {
+      if (publicKeyIn && isMounted) {
+        dispatch(CollectiblesLoadAction(publicKeyIn!));
+      }
+    }, [isMounted, dispatch, publicKeyIn]);
+  
+    const loadNextPage = useCallback(() => {
+      if (publicKeyIn) {
+        const { page } = query ?? { page: 1 };
+        dispatch(
+          CollectiblesLoadAction(publicKeyIn!, {
+            ...query,
+            page: page! + 1,
+          })
+        );
+      }
+    }, [query, dispatch, publicKeyIn]);
+  
+    const flatAssets = useMemo(() => assets.flat(), [assets]);
+  
+    const onCardsScroll = useCallback(
+      ({ target }) => {
+        const isBottom =
+          target.scrollHeight - target.scrollTop === target.clientHeight;
+        if (isBottom && !isLastPage && !isLoading) {
+          loadNextPage();
+        }
+      },
+      [loadNextPage, isLastPage, isLoading]
+    );
+  
+    const cardColSize = useCallback(() => {
+      if (isLarge) {
+        return 3;
+      }
+      if (isMedium) {
+        return 4;
+      }
+      return 6;
+    }, [isMedium, isLarge]);
+    
   return (
+    <>
     <IonGrid className="ion-no-padding">
       <IonRow>
         <ImageBgCol size="12">
@@ -92,9 +205,103 @@ const AuctionSell: FC = () => {
             since some disappear at a racing speed!
           </Text>
           <br />
+          <StartAuctionButton
+                className="ion-float-left"
+                color="#F8C938"
+                fill="clear"
+                fontSize={FontSize.SM}
+                fontWeight={FontWeight.BOLD}
+                onClick={() => StartAuction()}
+              >
+                Start Auction
+          </StartAuctionButton>   
+          <CardsOnAuctionButton
+                className="ion-center"
+                color="light"
+                fill="clear"
+                fontSize={FontSize.SM}
+                fontWeight={FontWeight.BOLD}
+                onClick={() => CardsOnAuction()}
+              >
+                Cards On Auction
+          </CardsOnAuctionButton> 
+          <BiddedCardsButton
+                className="ion-float-right"
+                color="light"
+                fill="clear"
+                fontSize={FontSize.SM}
+                fontWeight={FontWeight.BOLD}
+                onClick={() => BiddedCards()}
+              >
+                Bidded Cards
+          </BiddedCardsButton>                            
         </ImageBgCol>
       </IonRow>
-    </IonGrid>
+      <CollectablesIonRow onScroll={onCardsScroll}>
+          {!isLoading && isError && (
+            <IonCol size="12" class="ion-text-center ion-padding-top">
+              <Text color="danger" fontSize={FontSize.XXL}>
+                Something went wrong!
+              </Text>
+              <Text
+                className="ion-padding-top"
+                fontSize={FontSize.SM}
+                color="light"
+              >
+                {error}
+              </Text>
+            </IonCol>
+          )}
+          {!isLoading && !!flatAssets.length && !isError && (
+            <>
+              {flatAssets.map(({ id, attributes }) => {
+                const {
+                  title,
+                  subtitle,
+                  ipfsHashImageFront,
+                  tags,
+                } = attributes as any;
+
+                const type =
+                  Array.isArray(tags) && tags.length
+                    ? tags[0]
+                    : CardTagType.Signed;
+
+                return (
+                  <CardContainer key={id} size={cardColSize().toString()}>
+                    <Card
+                      id={id}
+                      title={title}
+                      subtitle={subtitle}
+                      imgIpfsHash={ipfsHashImageFront}
+                      type={type}
+                    />
+                  </CardContainer>
+                );
+              })}
+            </>
+          )}
+          {!isLoading && !flatAssets.length && !isError && (
+            <IonCol size="12" class="ion-text-center">
+              <Text
+                className="ion-padding-top"
+                fontSize={FontSize.L}
+                color="primary"
+              >
+                No collectables yet!
+                <br/>
+                {publicKey}
+              </Text>
+            </IonCol>
+          )}
+          {isLoading && !flatAssets.length && (
+            <IonCol size="12" class="ion-text-center ion-padding-top">
+              <IonSpinner color="primary" />
+            </IonCol>
+          )}
+        </CollectablesIonRow>      
+    </IonGrid>   
+    </>
   );
 };
 
