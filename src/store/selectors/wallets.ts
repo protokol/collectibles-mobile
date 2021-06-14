fb480eadf64000badcfec99d60273e2e9642a9e6
Carlos Mayora <cmayorga@buclelabs.com>
2021-06-14 11:57:11 +0200

    feat: added environment configuration for faucet

--- src/store/selectors/wallets.ts
+++ src/store/selectors/wallets.ts
@@ -1,4 +1,4 @@
 import { RootState } from '../types';
 
 export const walletsSelector = ({ wallets: { wallets } }: RootState) => wallets;
-export const faucetSelector = ({ faucet: faucet }: RootState) => faucet;
+export const faucetSelector = ({ faucet }: RootState) => faucet;
